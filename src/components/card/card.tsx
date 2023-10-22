4913bb893004d39529689c6e0066d989572fed28
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2023-10-22 14:27:18 -0300

    ✨ feat: Construindo Card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,9 +1,17 @@
+import "./card.css"
 interface CardProps{
-
+    price:number,
+    title:string,
+    image:string
 }
 
-export function Card(props : CardProps){
-    return(
-        <></>
+export function Card({ price, image, title }: CardProps) {
+    return (
+        <div className="card">
+            <img src={image} alt={title} /> {/* Use src e alt para a tag img */}
+            <h2>{title}</h2> {/* Exiba o título */}
+            <p><b>Valor:</b> {price}</p> {/* Exiba o preço */}
+        </div>
     )
 }
+
